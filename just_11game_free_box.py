425f92ce7825c85af6c783055df9b679dbc20896
just_king <31703404+justsoso12@users.noreply.github.com>
2023-05-29 22:11:36 +0800

    Update just_11game_free_box.py

--- just_11game_free_box.py
+++ just_11game_free_box.py
@@ -16,6 +16,7 @@ ispush = True
 username = os.environ["name_11game"]
 password = os.environ["pwd_11game"]
 all_dict = {}
+indexed = []
 st = ""
 tgw_l7_route = ""
 cookie = ""
@@ -113,6 +114,164 @@ def getcharglist():
     else:
         return False
 
+def gettaskinfos():
+    gettaskinfos_url = f"https://actnp.5211game.com/newplayground/BZ/GetTaskInfos?st={st}"
+    gettaskinfos_head = {
+        "Host": "actnp.5211game.com",
+        "Connection": "keep-alive",
+        "sec-ch-ua": '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+        "Accept": "application/json, text/plain, */*",
+        "sec-ch-ua-mobile": "?0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+        "sec-ch-ua-platform": '"Windows"',
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Dest": "empty",
+        "Referer": "https://actnp.5211game.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+        "cookie": "tgw_l7_route=" + tgw_l7_route + ";loginInfo=" + st
+    }
+    gettaskinfos_response = session.get(url=gettaskinfos_url,headers=gettaskinfos_head)
+    if gettaskinfos_response.status_code == 200 and "操作成功" in gettaskinfos_response.text:
+        msgbox("【宝藏每日任务】领取情况：")
+        for tasks in gettaskinfos_response.json()['data']['lstDayTasks']:
+            if tasks['flag'] == 0:
+                if dealtaskinfos(tasks['task_id']):
+                    msgbox(f"{tasks['task_name']} 任务进度（{tasks['V']}/{tasks['C']}）领取成功！")
+                else:
+                    msgbox(f"{tasks['task_name']} 任务进度（{tasks['V']}/{tasks['C']}）领取失败！")
+            elif tasks['flag'] == 1:
+                msgbox(f"{tasks['task_name']} 任务进度（{tasks['V']}/{tasks['C']}）已经领取！")
+            else:
+                msgbox(f"{tasks['task_name']} 任务进度（{tasks['V']}/{tasks['C']}）未达标！")
+        msgbox("【宝藏每周任务】领取情况：")
+        for tasks in gettaskinfos_response.json()['data']['lstWeekTasks']:
+            if tasks['flag'] == 0:
+                if dealtaskinfos(tasks['task_id']):
+                    msgbox(f"{tasks['task_name']} 任务进度（{tasks['V']}/{tasks['C']}）领取成功！")
+                else:
+                    msgbox(f"{tasks['task_name']} 任务进度（{tasks['V']}/{tasks['C']}）领取失败！")
+            elif tasks['flag'] == 1:
+                msgbox(f"{tasks['task_name']} 任务进度（{tasks['V']}/{tasks['C']}）已经领取！")
+            else:
+                msgbox(f"{tasks['task_name']} 任务进度（{tasks['V']}/{tasks['C']} ）未达标！")
+        return True
+    else:
+        return False
+
+def dealtaskinfos(tid):
+    dealtaskinfos_url = f"https://actnp.5211game.com/newplayground/BZ/RcvTaskGift?st={st}&tid={tid}"
+    dealtaskinfos_head = {
+        "Host": "actnp.5211game.com",
+        "Connection": "keep-alive",
+        "sec-ch-ua": '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+        "Accept": "application/json, text/plain, */*",
+        "sec-ch-ua-mobile": "?0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+        "sec-ch-ua-platform": '"Windows"',
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Dest": "empty",
+        "Referer": "https://actnp.5211game.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+        "cookie": "tgw_l7_route=" + tgw_l7_route + ";loginInfo=" + st
+    }
+    dealtaskinfos_response = session.get(url=dealtaskinfos_url,headers=dealtaskinfos_head)
+    if dealtaskinfos_response.status_code == 200 and "操作成功" in dealtaskinfos_response.text:
+        return True
+    else:
+        return False
+
+def getgridtimes():
+    global indexed
+    getgridtimes_url = f"https://actnp.5211game.com/newplayground/BZ/GetGridTimes?st={st}"
+    getgridtimes_head = {
+        "Host": "actnp.5211game.com",
+        "Connection": "keep-alive",
+        "sec-ch-ua": '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+        "Accept": "application/json, text/plain, */*",
+        "sec-ch-ua-mobile": "?0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+        "sec-ch-ua-platform": '"Windows"',
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Dest": "empty",
+        "Referer": "https://actnp.5211game.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+        "cookie": "tgw_l7_route=" + tgw_l7_route + ";loginInfo=" + st
+    }
+    getgridtimes_response = session.get(url=getgridtimes_url,headers=getgridtimes_head)
+    if getgridtimes_response.status_code == 200 and "操作成功" in getgridtimes_response.text:
+        times = getgridtimes_response.json()['data']
+        msgbox(f"【宝藏可开箱次数】 {times}次")
+        if times >0:
+            for a in range(0,times):
+                for b in range(1,51):
+                    if b not in indexed:
+                        if openbox(b):
+                            indexed.append(b)
+                        break
+
+def openbox(index):
+    openbox_url = f"https://actnp.5211game.com/newplayground/BZ/RcvGridGift?st={st}&index={index}"
+    openbox_head = {
+        "Host": "actnp.5211game.com",
+        "Connection": "keep-alive",
+        "sec-ch-ua": '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+        "Accept": "application/json, text/plain, */*",
+        "sec-ch-ua-mobile": "?0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+        "sec-ch-ua-platform": '"Windows"',
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Dest": "empty",
+        "Referer": "https://actnp.5211game.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+        "cookie": "tgw_l7_route=" + tgw_l7_route + ";loginInfo=" + st
+    }
+    openbox_response = session.get(url=openbox_url,headers=openbox_head)
+    if openbox_response.status_code == 200 and "操作成功" in openbox_response.text:
+        msgbox(f"【宝藏开宝箱】获取到【{openbox_response.json()['data']['show_name']}】")
+        return True
+    else:
+        return False
+
+
+
+def boxdetail():
+    global indexed
+    boxdetail_url = f"https://actnp.5211game.com/newplayground/BZ/GetUserGridDetail?st={st}"
+    boxdetail_head = {
+        "Host": "actnp.5211game.com",
+        "Connection": "keep-alive",
+        "sec-ch-ua": '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+        "Accept": "application/json, text/plain, */*",
+        "sec-ch-ua-mobile": "?0",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+        "sec-ch-ua-platform": '"Windows"',
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Dest": "empty",
+        "Referer": "https://actnp.5211game.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+        "cookie": "tgw_l7_route=" + tgw_l7_route + ";loginInfo=" + st
+    }
+    boxdetial_response = session.get(url=boxdetail_url,headers=boxdetail_head)
+    if boxdetial_response.status_code == 200 and "操作成功" in boxdetial_response.text:
+        msgbox("【宝藏宝箱】开启情况：")
+        for dic in boxdetial_response.json()['data']:
+            if dic['is_opened']:
+                indexed.append(dic['index'])
+                msgbox(f"【宝藏宝箱】index:{dic['index']} {dic['show_name']} 已开启！")
+        return True
+    else:
+        return False
+
 def getfreebox():
     global all_dict
     getfreebox_url = f"https://actnp.5211game.com/newplayground/RS/RcvDayOne?st={st}"
@@ -137,6 +296,30 @@ def getfreebox():
         return True
     else:
         return False
+
+def getdata():
+    global cookie
+    getdata_url = "https://actdota.5211game.com/LBPS202351/JP/GetData"
+    getdata_head = {
+        "host": "actdota.5211game.com",
+        "sec-ch-ua": '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+        "accept": "*/*",
+        "x-requested-with": "XMLHttpRequest",
+        "sec-ch-ua-mobile": "?0",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+        "sec-ch-ua-platform": '"Windows"',
+        "origin": "https://actdota.5211game.com",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-dest": "empty",
+        "referer": "https://actdota.5211game.com/LBPS202351",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+        "cookie": cookie
+    }
+    getdata_response = session.post(url=getdata_url,headers=getdata_head)
+    if getdata_response.status_code == 200:
+        print(getdata_response.text)
 
 def gettaskdata(tp):
     global cookie
@@ -237,19 +420,23 @@ def msgbox(str):
 
 if __name__ == '__main__':
   if login(username,password):
-
       print("【11Game用户】" + all_dict["userName"] + " 登陆成功！")
       if getuserinfo():
           msgbox("【11Game用户】" + all_dict["userName"] + " 信息获取成功！")
           if getcharglist():
+              msgbox("【碎片数量】统计：")
               msgbox("【荣耀精粹】" + all_dict["honor_essence"])
-              msgbox("【限定碎片】" + all_dict["limit_skin_chip"])
+              # msgbox("【限定碎片】" + all_dict["limit_skin_chip"])
               msgbox("【宝箱碎片】" + all_dict["box_chip"])
               msgbox("【皮肤碎片】" + all_dict["skin_essence"])
+          if gettaskinfos():
+              print("【宝藏任务执行完毕】")
+          if boxdetail():
+              getgridtimes()
           if getfreebox():
-              msgbox("【开宝箱结果】" + all_dict["box_msg"])
+              msgbox("【每日免费宝箱】" + all_dict["box_msg"])
           if getcookie():
-              print("【任务获取及奖励领取】")
+              print("【夏季通行证任务】领取情况：")
               for tp in range(1,5):
                   gettaskdata(tp)
           msgbox("【任务执行完毕】")
